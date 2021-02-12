35afdd2aed0a8fa17af0835204d932f7a98458e0
Kimberly Dauber <noteflightthemes@gmail.com>
2021-02-11 20:51:57 -0800

    allow user to choose automata, ew

--- src/model/axioms/Automata.ts
+++ src/model/axioms/Automata.ts
@@ -1,16 +1,17 @@
-import Axiom, { FlavorList } from "../Axiom";
+import Axiom, { AxiomFlavorList } from "../Axiom";
+import Flavor from "../Flavor";
 import QuirkTemplate from "../QuirkTemplate";
 
 class Automata extends Axiom {
-    public static readonly ID = "apo";
+    public static readonly ID = "aut";
     public static readonly DISPLAY_NAME = "Automata";
 
-    public static readonly FLAVOR_LIST: FlavorList = [
-        [],
-        [],
-        [],
-        [],
-        []
+    public static readonly FLAVOR_LIST: AxiomFlavorList = [
+        [Flavor.createFlavor("computer", "Computer")],
+        [Flavor.createFlavor("zombie", "Zombie")],
+        [Flavor.createFlavor("dog", "Dog")],
+        [Flavor.createFlavor("person", "Person")],
+        [Flavor.createFlavor("downfall", "Your Downfall")]
     ];
 
     public static readonly QUIRK_TEMPLATES: QuirkTemplate[] = [];
